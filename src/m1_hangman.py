c881b4376792db0c8dc2cc22d75efd11163879f1
wohlfab <wohlfab@rose-hulman.edu>
2019-01-28 14:46:31 -0500

    Class 22

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,10 +1,14 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
+Authors: Brandon Wohlfarth and Jacob Tebbe.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
 
 ####### Do NOT attempt this assignment before class! #######
+min_length = 7
+how_many_guesses = 5
+
+while True:
 
